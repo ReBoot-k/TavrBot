e6976aac4a0cf81ec192af50abf1d3d161715034
Михаил <131132246+ReBoot-k@users.noreply.github.com>
2023-09-11 16:47:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from pathlib import Path
 TOKEN = ""
 GROUP_ID = 0
 
-FILENAME_ALL = Path("data\\all.xlsx")
-FILENAME_CORRECTION = Path("data\\correction.xlsx")
+FILENAME_ALL = Path("data/all.xlsx")
+FILENAME_CORRECTION = Path("data/correction.xlsx")
